7d496bd538398ab1a2ff5aabe7a53d22741fe903
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2020-11-10 13:51:21 +0100

    Create UserModel.js

---
+++ server/UserModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String
+})
+
+const User = mongoose.model('User', userSchema);
+
+exports.createUser = (inName, inPassword) =>{
+    let user = new User({
+        name: inName,
+        password: inPassword
+    })
+    return user;
+}
